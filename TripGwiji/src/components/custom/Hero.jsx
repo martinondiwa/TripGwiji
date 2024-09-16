8c0bd2a8f4e64e5443e79b2a73846cab448388df
martinondiwa <surekondiwa@gmail.com>
2024-09-16 16:09:36 +0300

    dfg

--- TripGwiji/src/components/custom/Hero.jsx
+++ TripGwiji/src/components/custom/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '../ui/button';
 
 
 function Hero() {
@@ -6,8 +7,9 @@ function Hero() {
         <>
             <div className='font-extrabold text-[50px] flex items-center mx-56 gap-9 text-center'>
             <span className='text-[#f56551]'>Discover Your Next Adventure with AI:</span> <br></br>
-                <h2 className='font-extrabold text-[50px] text-center mt-6'>Plan your perfect getaway with our AI-powered trip planner, designed to tailor every detail of your journey to your unique preferences and needs.</h2>
+                <p className='font-extrabold text-[50px] text-center mt-6'>Plan your perfect getaway with our AI-powered trip planner, designed to tailor every detail of your journey to your unique preferences and needs.</p>
             </div>
+            <Button>Get Started, It's Free</Button>
         </>
     )
 }
