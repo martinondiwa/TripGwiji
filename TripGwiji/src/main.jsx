3369dfd8eee901d38218adca6221563d850923be
martinondiwa <surekondiwa@gmail.com>
2024-09-16 12:29:05 +0300

    create

--- TripGwiji/src/main.jsx
+++ TripGwiji/src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter } from 'react-router-dom'
+import CreateTrip from './create-trip/index.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
   },
+  {
+    path:'/CreateTrip',
+    element: <CreateTrip/>
+  }
 ]);
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
