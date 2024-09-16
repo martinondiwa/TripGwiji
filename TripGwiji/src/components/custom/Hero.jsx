0f5db22f12c30d0d0a2024d8f6f16dde9d644a5d
martinondiwa <surekondiwa@gmail.com>
2024-09-16 15:33:28 +0300

    tag

--- TripGwiji/src/components/custom/Hero.jsx
+++ TripGwiji/src/components/custom/Hero.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import { Button } from '../ui/button'
 
 function Hero() {
-  return (
-    <div className='font-extrabold text-[60px]'>
-    <h2>Plan your perfect getaway with our AI-powered trip planner, designed to tailor every detail of your journey to your unique preferences and needs.</h2>
-    </div>
-   
-  )
+    return (
+        <>
+            <div className='font-extrabold text-[60px]'>
+                <h2>Plan your perfect getaway with our AI-powered trip planner, designed to tailor every detail of your journey to your unique preferences and needs.</h2>
+            </div>
+        </>
+    )
 }
 
 export default Hero
--- TripGwiji/src/components/custom/header.jsx
+++ TripGwiji/src/components/custom/header.jsx
@@ -3,12 +3,15 @@ import { Button } from '../ui/button'
 
 function Header() {
   return (
-    <div className='p-3 shadow-sm flex justify-between items-center px-5'>
-    <img src='/logo.svg'/>
-    </div>
-    <div>
-    <Button>Sign in Button</Button>
-    </div>
+    <>
+      <div className='p-3 shadow-sm flex justify-between items-center px-5'>
+        <img src='/logo.svg' />
+      </div>
+
+      <div>
+        <Button>Sign in Button</Button>
+      </div>
+    </>
   )
 }
 
