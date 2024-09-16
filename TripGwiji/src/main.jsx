12e58c744ce41efab246f899e4d675f0fd1a1107
martinondiwa <surekondiwa@gmail.com>
2024-09-16 12:17:13 +0300

    dd

--- TripGwiji/src/main.jsx
+++ TripGwiji/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+  <RouterProvider router={router} />
+</React.StrictMode>
 )
