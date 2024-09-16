3c06fbac591348cf7b0f6392dba244da332312a1
martinondiwa <surekondiwa@gmail.com>
2024-09-16 15:42:19 +0300

    ghj

--- TripGwiji/src/components/custom/Hero.jsx
+++ TripGwiji/src/components/custom/Hero.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import './App.css'
-import { Button } from '../ui/button'
 
 function Hero() {
     return (
         <>
-            <div className='font-extrabold text-[60px]'>
+            <div className='font-extrabold text-[60px] flex items-center mx-56 gap-9'>
                 <h2>Plan your perfect getaway with our AI-powered trip planner, designed to tailor every detail of your journey to your unique preferences and needs.</h2>
             </div>
         </>
