bf3b8af39ae4648189d2bd33c093b8c7586b5b75
martinondiwa <surekondiwa@gmail.com>
2024-09-17 16:22:00 +0300

    people

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -47,15 +47,14 @@ function CreateTrip() {
           {SelectBudgetOptions.map((item, index) => (
             <div key={index}
               onClick={() => handleInputChange('budget', item.title)}
-              className={'p-4 border rounded-lg hover:shadow-lg cursor-pointer
-               ${formData?.budget == item.title && 'shadow-lg border-black'}
-              '}>
-            < h2 className = 'text-4xl' > { item.icon }</h2>
-        <h2 className='font-bold text-lg'>{item.title}</h2>
-        <h2 className='text-sm text-blue-500'>{item.desc}</h2>
-      </div>
+              className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer
+                ${formData?.budget === item.title ? 'shadow-lg border-black' : ''}`}>
+              < h2 className='text-4xl' > {item.icon}</h2>
+              <h2 className='font-bold text-lg'>{item.title}</h2>
+              <h2 className='text-sm text-blue-500'>{item.desc}</h2>
+            </div>
           ))}
-    </div>
+        </div>
       </div >
 
 
@@ -64,20 +63,20 @@ function CreateTrip() {
         <div className='grid grid-cols-3 gap-5 mt-5'>
           {SelectTravelesList.map((item, index) => (
             <div key={index}
-            onClick={()=> handleInputChange('traveler',item.people)}
-            className= {'p-4 border rounded-lg hover:shadow-lg cursor-pointer
-              ${formData?.traveler==item.people&&'shadow-lg border-black'}
-              '}>
-              <h2 className='text-4xl'>{item.icon}</h2>
+
+              onClick={() => handleInputChange('traveler', item.people)}
+              className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer
+                ${formData?.traveler== item.people ?'shadow-lg border-black' : ''}`}>
+              < h2 className='text-4xl' > {item.icon}</h2>
               <h2 className='font-bold text-lg'>{item.title}</h2>
               <h2 className='text-sm text-blue-500'>{item.desc}</h2>
             </div>
           ))}
         </div>
-      </div>
+      </div >
 
       <div className='my-10 justify-end flex'>
-      <Button>Generate Trip</Button> 
+        <Button>Generate Trip</Button>
       </div>
     </div >
 
