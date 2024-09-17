555a48529f1ba04ee7a14e3d1674c651dbc42bd2
martinondiwa <surekondiwa@gmail.com>
2024-09-17 15:10:18 +0300

    camping

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -10,7 +10,7 @@ function CreateTrip() {
   const [place, setPlace] = useState()
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
-      <h2 className='font-bold text-3xl'>Tell us your travel preferences </h2>
+      <h2 className='font-bold text-3xl'>Tell us your travel preferences 🏕️</h2>
       <p className='mt-4 text-gray-500 text-xl'>Help us customize your trip by sharing a few details about your travel style. Whether you prefer adventure, relaxation, or cultural experiences, we’ve got something for you. Your preferences will guide us in planning the perfect trip tailored just for you.</p>
       <div>
         <div className='mt-20'>
