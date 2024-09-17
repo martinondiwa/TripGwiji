ce371f0258573a5148289fcb6ed62878a5054711
martinondiwa <surekondiwa@gmail.com>
2024-09-17 13:26:42 +0300

    id1

--- TripGwiji/src/constants/options.jsx
+++ TripGwiji/src/constants/options.jsx
@@ -2,5 +2,12 @@ export const SelectTravelesList =[
     {
         id:1,
         title:'Just me',
+        desc: 'A sole travelers in exploration',
+        icon:'+',
+        people:'1'
+        
+    },
+    {
+        
     }
 ]
