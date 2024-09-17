4c42077b65aacedabc9df8753a7c9e4c79c388d0
martinondiwa <surekondiwa@gmail.com>
2024-09-17 12:48:35 +0300

    setplace

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 
 function CreateTrip() {
+  const [place,setPlace]= useState()
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>Tell us your travel preferences </h2>
@@ -12,7 +13,7 @@ function CreateTrip() {
           <h2 className='text-xl my-3 font-medium'>What is your destination of choice?</h2>
            <GooglePlacesAutocomplete apiKey={'import.meta.env.VITE_GOOGLE_PLACE_API_KEY'}
            selectProps={{
-            
+
            }}
            />
         </div>
