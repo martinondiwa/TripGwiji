fdc26653110d3305ff0398c4ebe125a02bcd9046
martinondiwa <surekondiwa@gmail.com>
2024-09-17 11:27:13 +0300

    padding

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CreateTrip() {
   return (
-      <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10'>Create trip</div>
+      <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>Create trip</div>
     
   )
 }
