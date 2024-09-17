f981ec9e268a9879b60e5d5ca57af919482abbca
martinondiwa <surekondiwa@gmail.com>
2024-09-17 11:53:19 +0300

    marg

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -6,7 +6,7 @@ function CreateTrip() {
       <h2 className='font-bold text-3xl'>Tell us your travel preferences </h2>
       <p className='mt-4 text-gray-500 text-xl'>Help us customize your trip by sharing a few details about your travel style. Whether you prefer adventure, relaxation, or cultural experiences, we’ve got something for you. Your preferences will guide us in planning the perfect trip tailored just for you.</p>
       <div>
-        <div>
+        <div className='mt-10'>
           <h2 className='text-xl my-3 font-medium'>What is your destination of choice?</h2>
         </div>
       </div>
