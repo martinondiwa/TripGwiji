589b9b4178a91e9a1799b4cc1ec20bc44efdc4b6
martinondiwa <surekondiwa@gmail.com>
2024-09-17 14:31:56 +0300

    text

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -33,7 +33,7 @@ function CreateTrip() {
             <div key={index} className='p-4 border rounded-lg hover:shadow-lg'>
             <h2 className='text-4xl'>{item.icon}</h2>
             <h2 className='font-bold text-lg'>{item.title}</h2>
-            <h2 className='text-sm text-gray-900'>{item.desc}</h2>
+            <h2 className='text-sm text-blue-500'>{item.desc}</h2>
             </div>
           ))}
         </div>
