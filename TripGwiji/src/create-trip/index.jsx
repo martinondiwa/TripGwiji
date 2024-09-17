2bff63842c8615451e917f09f48701925c97d3a3
martinondiwa <surekondiwa@gmail.com>
2024-09-17 12:06:08 +0300

    goog

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -10,7 +10,7 @@ function CreateTrip() {
       <div>
         <div className='mt-20'>
           <h2 className='text-xl my-3 font-medium'>What is your destination of choice?</h2>
-           <GooglePlacesAutocomplete/>
+           <GooglePlacesAutocomplete apiKey=''/>
         </div>
       </div>
 
