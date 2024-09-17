6e8bf88dd3706fbcad00b768295c1e35e712f179
martinondiwa <surekondiwa@gmail.com>
2024-09-17 19:48:05 +0300

    googleAi

--- TripGwiji/src/service/AIModal.jsx
+++ TripGwiji/src/service/AIModal.jsx
@@ -1,11 +1,8 @@
 
-const {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } = require("@google/generative-ai");
-  
-  const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_AI_API_KEY;
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_AI_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
