2fe555e56237e477e394b0c26f618c7fcdec0a88
martinondiwa <surekondiwa@gmail.com>
2024-09-17 09:07:33 +0300

    error page

--- TripGwiji/src/App.jsx
+++ TripGwiji/src/App.jsx
@@ -1,7 +1,8 @@
-import react, { useState } from 'react'; // Correct import
+import React from 'react'
 import './App.css';
 import { Button } from './components/ui/button';
-import Hero from './components/Hero'; // Make sure the path is correct
+import Hero from './components/custom/Hero';
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       {/* Hero */}
-      <Hero />
+      <Hero/>
     </>
   );
 }
