3b6caee5d49ed05f4dfd05649ca7735d0ecfdf50
martinondiwa <surekondiwa@gmail.com>
2024-09-17 11:32:05 +0300

    trip

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function CreateTrip() {
   return (
-      <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>Create trip</div>
+      <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
+      <h2>Tell us your travel preferences </h2>
+      <p>Help us customize your trip by sharing a few details about your travel style. Whether you prefer adventure, relaxation, or cultural experiences, we’ve got something for you. Your preferences will guide us in planning the perfect trip tailored just for you.</p>
+      </div>
     
   )
 }
