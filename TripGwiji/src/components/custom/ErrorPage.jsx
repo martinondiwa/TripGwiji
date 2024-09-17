08b65b1531b60211aa786d22bd8e1509c7477e16
martinondiwa <surekondiwa@gmail.com>
2024-09-17 09:45:59 +0300

    error

--- TripGwiji/src/components/custom/ErrorPage.jsx
+++ TripGwiji/src/components/custom/ErrorPage.jsx
@@ -1,3 +1,7 @@
+import { useRouteError } from "react-router-dom";
+
 export default function ErrorPage(){
+    const error = useRouteError();
+    console.log(error);
     return <div>The page you are trying is not available</div>;
 }
