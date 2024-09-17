99822516bd80f98483245de493ba1a49614eb15a
martinondiwa <surekondiwa@gmail.com>
2024-09-17 14:38:27 +0300

    tra

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -26,14 +26,29 @@ function CreateTrip() {
           <Input placeholder={'Ex.3'} type="number" />
         </div>
       </div>
+
       <div>
         <h2 className='font-bold text-3xl'>What is you Budget?</h2>
         <div className='grid grid-cols-3 gap-5 mt-5'>
           {SelectBudgetOptions.map((item, index) => (
             <div key={index} className='p-4 border rounded-lg hover:shadow-lg cursor-pointer'>
-            <h2 className='text-4xl'>{item.icon}</h2>
-            <h2 className='font-bold text-lg'>{item.title}</h2>
-            <h2 className='text-sm text-blue-500'>{item.desc}</h2>
+              <h2 className='text-4xl'>{item.icon}</h2>
+              <h2 className='font-bold text-lg'>{item.title}</h2>
+              <h2 className='text-sm text-blue-500'>{item.desc}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+
+
+      <div>
+        <h2 className='font-bold text-3xl'>Who are you planning to travel with?</h2>
+        <div className='grid grid-cols-3 gap-5 mt-5'>
+          {SelectBudgetOptions.map((item, index) => (
+            <div key={index} className='p-4 border rounded-lg hover:shadow-lg cursor-pointer'>
+              <h2 className='text-4xl'>{item.icon}</h2>
+              <h2 className='font-bold text-lg'>{item.title}</h2>
+              <h2 className='text-sm text-blue-500'>{item.desc}</h2>
             </div>
           ))}
         </div>
