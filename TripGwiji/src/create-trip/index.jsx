e4eb0e7d3e3b6227fe1d55aef0cad87cbbb629c7
martinondiwa <surekondiwa@gmail.com>
2024-09-17 15:15:58 +0300

    form

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -7,7 +7,9 @@ import { Button } from '@/components/ui/button';
 
 
 function CreateTrip() {
-  const [place, setPlace] = useState()
+  const [place, setPlace] = useState();
+
+  const [formData,setFormData]=useState([]);
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>Tell us your travel preferences 🏕️🌴</h2>
