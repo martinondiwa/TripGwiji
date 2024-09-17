dcb86723942dec0b258a703634e93d66e57e68d8
martinondiwa <surekondiwa@gmail.com>
2024-09-17 14:29:19 +0300

    text

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -31,8 +31,8 @@ function CreateTrip() {
         <div className='grid grid-cols-3 gap-5 mt-5'>
           {SelectBudgetOptions.map((item, index) => (
             <div key={index} className='p-4 border rounded-lg hover:shadow-lg'>
-            <h2 className='text-3xl'>{item.icon}</h2>
-            <h2>{item.title}</h2>
+            <h2 className='text-4xl'>{item.icon}</h2>
+            <h2 className='font-bold text-lg'>{item.title}</h2>
             <h2>{item.desc}</h2>
             </div>
           ))}
