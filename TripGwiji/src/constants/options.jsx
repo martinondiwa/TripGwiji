fa7f2b2a8fc8347e4e3d6850964e7d6f361b3de8
martinondiwa <surekondiwa@gmail.com>
2024-09-17 13:55:27 +0300

    budget

--- TripGwiji/src/constants/options.jsx
+++ TripGwiji/src/constants/options.jsx
@@ -1,45 +1,56 @@
 
-export const SelectTravelesList =[
+export const SelectTravelesList = [
     {
-        id:1,
-        title:'Just me',
+        id: 1,
+        title: 'Just me',
         desc: 'A sole travelers in exploration',
-        icon:'+',
-        people:'1'
-        
-    },
-    {
-      id:2,
-      title:'Couple',
-      desc:'Two travelers in tandem',
-      icon: '🥂🥂',
-      people:'2 people'
-    },
-    {
-      id:3,
-      title:'Family',
-      desc: 'A group of fun loving adventurers',
-      icon:'👨‍👩‍👧‍👦',
-      people: '2-10 People'
+        icon: '+',
+        people: '1'
 
     },
     {
-        id:4,
-        title:'Friends',
+        id: 2,
+        title: 'Couple',
+        desc: 'Two travelers in tandem',
+        icon: '🥂🥂',
+        people: '2 people'
+    },
+    {
+        id: 3,
+        title: 'Family',
+        desc: 'A group of fun loving adventurers',
+        icon: '👨‍👩‍👧‍👦',
+        people: '2-10 People'
+
+    },
+    {
+        id: 4,
+        title: 'Friends',
         desc: 'A bunch of thrill-seekers',
-        icon:'👫👬',
+        icon: '👫👬',
         people: '5 to 10'
-        
+
     }
-]
+];
 
-export const SelectBudgetOptions=[
-
+export const SelectBudgetOptions = [
     {
-        id:1,
-        title:'Cheap',
+        id: 1,
+        title: 'Cheap',
         desc: 'Stay conscious of costs',
-        icon:''
+        icon: '💸' // Money with wings emoji
+    },
+    {
+        id: 2,
+        title: 'Moderate',
+        desc: 'A balance of cost and comfort',
+        icon: '💵' // Dollar banknote emoji
+    },
+    {
+        id: 3,
+        title: 'Luxury',
+        desc: 'Indulge in high-end experiences',
+        icon: '💰' // Money bag emoji
     }
 
-]
+];
