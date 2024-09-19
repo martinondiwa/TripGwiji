f081cba5a0990aa843fc997cfebdb109d23fd677
martinondiwa <surekondiwa@gmail.com>
2024-09-19 10:56:30 +0300

    ful

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -124,7 +124,7 @@ function CreateTrip() {
               <img src="/logo.svg"/>
               <h2 className="font-bold text-lg mt-7 text-lime-400">Sign in with google</h2>
               <p>Sign in to the app with google Authentication securely</p>
-              <Button ClassName="">Sign in with Google</Button>
+              <Button className="w-full mt-5">Sign in with Google</Button>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
