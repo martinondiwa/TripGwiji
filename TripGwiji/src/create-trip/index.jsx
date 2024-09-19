11748e5f50a6da18e13025e7598ae91f32913059
martinondiwa <surekondiwa@gmail.com>
2024-09-19 14:14:53 +0300

    load

--- TripGwiji/src/create-trip/index.jsx
+++ TripGwiji/src/create-trip/index.jsx
@@ -170,7 +170,9 @@ function CreateTrip() {
         </div>
 
         <div className='my-10 justify-end flex'>
-          <Button className='bg-green-500 hover:bg-green-600 text-white px-6 py-3 rounded-lg shadow-lg transition duration-300 ease-in-out' onClick={OnGenerateTrip}>
+          <Button 
+          disabled ={loading}
+          className='bg-green-500 hover:bg-green-600 text-white px-6 py-3 rounded-lg shadow-lg transition duration-300 ease-in-out' onClick={OnGenerateTrip}>
            {
           loading?
            <AiOutlineLoading3Quarters className='h-7 w-7 animate-spin' /> : 'Generate Trip'
