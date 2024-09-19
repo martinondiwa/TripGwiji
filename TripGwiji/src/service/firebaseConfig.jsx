0f2d45aa2453267a79fb52b3f5a94d5e6f33f4bf
martinondiwa <surekondiwa@gmail.com>
2024-09-19 13:20:47 +0300

    fire

--- TripGwiji/src/service/firebaseConfig.jsx
+++ TripGwiji/src/service/firebaseConfig.jsx
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirebase} from "firebase/firestore"
+import { getFirestore } from "firebase/firestore"; 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db=getFirestore(app);
+export const db = getFirestore(app); 
 
-//const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
