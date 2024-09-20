fe0ddc392dde2bb9f2238e9535816c9a0841e024
martinondiwa <surekondiwa@gmail.com>
2024-09-20 14:12:52 +0300

    grt

--- TripGwiji/src/View-trip/components/Hotels.jsx
+++ TripGwiji/src/View-trip/components/Hotels.jsx
@@ -4,13 +4,10 @@ function Hotels(trip) {
   return (
     <div>
       <h2 className='font-bold text-xl mt-5'>Hotel Recommendations</h2>
-      <div>
-        {trip?.tripData?.hotels?.map((item, index) => (
-          <div key={index}>
-            <img
-              src="placeholder.JPG"
-              className="rounded-lg"
-            />
+      <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
+        {trip?.tripData?.hotels?.map((item, index)=>(
+          <div>
+            <img src='/TripGwiji/public/hotelplaceholder.JPG'/>
           </div>
         ))}
       </div>
