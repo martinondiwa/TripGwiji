4eb3bb603dddef5c43d94d36a2284e5b4cb7d17f
martinondiwa <surekondiwa@gmail.com>
2024-09-20 14:42:33 +0300

    hotel

--- TripGwiji/src/View-trip/components/Hotels.jsx
+++ TripGwiji/src/View-trip/components/Hotels.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { LuMapPin } from "react-icons/lu";
 
 function Hotels({ trip }) {
   return (
     <div>
       <h2 className='font-bold text-xl mt-5'>Hotel Recommendations</h2>
       <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5'>
-        {trip?.tripData?.hotels?.map((item, index) => (
+        {trip?.tripData?.hotels?.map((hotel, index) => (
           <div key={index}>
             <img 
               src='/placeholder.JPG' 
@@ -13,7 +14,8 @@ function Hotels({ trip }) {
               className='rounded-lg'
             />
             <div>
-              <h2 className='my-3'>{hotel.hotelName}</h2>
+              <h2 className='font-medium'>{hotel.hotelName}</h2>
+              <h2 className='text-xs text-gray-500'><LuMapPin />{hotel.hotelName}</h2>
             </div>
           </div>
         ))}
