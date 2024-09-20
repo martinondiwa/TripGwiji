691f51281c3842ae0050b21ce5bd0d8a98a91398
martinondiwa <surekondiwa@gmail.com>
2024-09-20 10:35:42 +0300

    use

--- TripGwiji/src/View-trip/[tripId]/index.jsx
+++ TripGwiji/src/View-trip/[tripId]/index.jsx
@@ -1,12 +1,18 @@
 import { toast } from '@/hooks/use-toast';
 import { db } from '@/service/firebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 function Viewtrip() {
 
   const {tripId}=useParams();
+
+  useEffect(()=>{
+    GetTripData();
+  },[])
+
+
   const GetTripData=async()=>{
     const docRef=doc(db, 'AITrips',tripId);
     const docSnap=await getDoc(docRef);
