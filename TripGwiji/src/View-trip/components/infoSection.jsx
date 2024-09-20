f86e713a0adb3dc4f856dd762d7ea220d14ef97c
martinondiwa <surekondiwa@gmail.com>
2024-09-20 12:20:29 +0300

    calender

--- TripGwiji/src/View-trip/components/infoSection.jsx
+++ TripGwiji/src/View-trip/components/infoSection.jsx
@@ -8,7 +8,7 @@ function InfoSection({ trip }) {
                 <h2 className='font-bold text-2xl'>{trip?.userSelection?.location}</h2>
             </div>
             <div className='flex gap-5'>
-                <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>{trip.userSelection?.noOfDays}Days</h2>
+                <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>🗓️{trip.userSelection?.noOfDays}Days</h2>
                 <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>{trip.userSelection?.budget} Budget</h2>
                 <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>No. Of Travelers:{trip.userSelection?.traveler}</h2>
             </div>
