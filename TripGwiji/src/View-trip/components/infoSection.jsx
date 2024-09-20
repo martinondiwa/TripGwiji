a68d7756752b7274c93da34060ed52dfff34aa39
martinondiwa <surekondiwa@gmail.com>
2024-09-20 11:19:43 +0300

    rounded

--- TripGwiji/src/View-trip/components/infoSection.jsx
+++ TripGwiji/src/View-trip/components/infoSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function InfoSection(trip) {
   return (
     <div>
-      <img src='/placeholder.JPG' className='h-[300px] w-full object-cover rounded'/>
+      <img src='/placeholder.JPG' className='h-[300px] w-full object-cover rounded-xl'/>
     </div>
   )
 }
