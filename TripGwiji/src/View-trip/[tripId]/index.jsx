2e6510062f98847444da3451d6a592b255aeedf8
martinondiwa <surekondiwa@gmail.com>
2024-09-20 10:25:37 +0300

    fire

--- TripGwiji/src/View-trip/[tripId]/index.jsx
+++ TripGwiji/src/View-trip/[tripId]/index.jsx
@@ -1,13 +1,14 @@
 import { db } from '@/service/firebaseConfig';
-import { doc } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
 function Viewtrip() {
 
   const {tripId}=useParams();
-  const GetTripData=()=>{
+  const GetTripData=async()=>{
     const docRef=doc(db, 'AITrips',tripId);
+    const docSnap=await getDoc(docRef);
   }
 
   return (
