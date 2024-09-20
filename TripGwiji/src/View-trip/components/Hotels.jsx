a0170f3d0c4e0ee1febb7f93d10b466a736df12f
martinondiwa <surekondiwa@gmail.com>
2024-09-20 13:29:18 +0300

    trip

--- TripGwiji/src/View-trip/components/Hotels.jsx
+++ TripGwiji/src/View-trip/components/Hotels.jsx
@@ -5,7 +5,9 @@ function Hotels(trip) {
     <div>
       <h2 className='font-bold text-xl mt-5'>Hotel Recommendations</h2>
       <div>
-       {trip?.tripData?.htels?.map}
+       {trip?.tripData?.htels?.map((item,index)=>(
+        
+       ))}
       </div>
     </div>
   )
