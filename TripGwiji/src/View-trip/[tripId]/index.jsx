3e0a7e928850b7d1cbb50c6e12ab886feb2e1cc1
martinondiwa <surekondiwa@gmail.com>
2024-09-20 10:22:20 +0300

    id

--- TripGwiji/src/View-trip/[tripId]/index.jsx
+++ TripGwiji/src/View-trip/[tripId]/index.jsx
@@ -1,9 +1,15 @@
+import { db } from '@/service/firebaseConfig';
+import { doc } from 'firebase/firestore';
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
 function Viewtrip() {
 
   const {tripId}=useParams();
+  const GetTripData=()=>{
+    const docRef=doc(db, 'AITrips',tripId);
+  }
+
   return (
     <div>
       Viewtrip: {tripId}
