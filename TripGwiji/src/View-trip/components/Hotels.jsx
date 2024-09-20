296bde2f68614fae75b2394245a2ddf001b87de3
martinondiwa <surekondiwa@gmail.com>
2024-09-20 14:21:23 +0300

    hotel

--- TripGwiji/src/View-trip/components/Hotels.jsx
+++ TripGwiji/src/View-trip/components/Hotels.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-function Hotels(trip) {
+function Hotels({ trip }) {
   return (
     <div>
       <h2 className='font-bold text-xl mt-5'>Hotel Recommendations</h2>
       <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5'>
-        {trip?.tripData?.hotels?.map((item, index)=>(
-          <div>
-            <img src='/TripGwiji/public/hotelplaceholder.JPG'/>
+        {trip?.tripData?.hotels?.map((item, index) => (
+          <div key={index}>
+            <img 
+              src='/placeholder.JPG' 
+              alt={`Hotel ${index}`} 
+              className='rounded-lg'
+            />
+            <div>
+              <h2>{hotel.hotelName}</h2>
+            </div>
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Hotels
+export default Hotels;
