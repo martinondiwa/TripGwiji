dddd7d0a1733bffbf80be98418e8c6bd3d6b6a69
martinondiwa <surekondiwa@gmail.com>
2024-09-20 12:31:34 +0300

    fol

--- TripGwiji/src/View-trip/components/infoSection.jsx
+++ TripGwiji/src/View-trip/components/infoSection.jsx
@@ -4,7 +4,7 @@ function InfoSection({ trip }) {
     return (
         <div >
             <img src='/placeholder.JPG' className='h-[300px] w-full object-cover rounded-xl' />
-            <div className=''>
+            <div className='my-5 flex flex-col gap-2'>
                 <h2 className='font-bold text-2xl'>{trip?.userSelection?.location}</h2>
             </div>
             <div className='flex gap-5'>
