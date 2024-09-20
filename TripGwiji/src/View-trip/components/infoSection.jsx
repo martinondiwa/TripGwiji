3eff6e73c22a5ce087e4d2c8fdef35e36acea2da
martinondiwa <surekondiwa@gmail.com>
2024-09-20 12:16:02 +0300

    com

--- TripGwiji/src/View-trip/components/infoSection.jsx
+++ TripGwiji/src/View-trip/components/infoSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function InfoSection({ trip }) {
     return (
-        <div>
+        <div className='flex gap-5'>
             <img src='/placeholder.JPG' className='h-[300px] w-full object-cover rounded-xl' />
             <div className=''>
                 <h2 className='font-bold text-2xl'>{trip?.userSelection?.location}</h2>
@@ -10,7 +10,7 @@ function InfoSection({ trip }) {
             <div>
                 <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>{trip.userSelection?.noOfDays}Days</h2>
                 <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>{trip.userSelection?.budget} Budget</h2>
-                <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>No. Of Traveler:{trip.userSelection?.traveler}</h2>
+                <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>No. Of Travelers:{trip.userSelection?.traveler}</h2>
             </div>
         </div>
     )
