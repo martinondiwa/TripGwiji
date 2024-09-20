0f2611df417353d284b1db71602bf41bc07c32c2
martinondiwa <surekondiwa@gmail.com>
2024-09-20 12:08:29 +0300

    file

--- TripGwiji/src/View-trip/components/infoSection.jsx
+++ TripGwiji/src/View-trip/components/infoSection.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-function InfoSection({trip}) {
-  return (
-    <div>
-      <img src='/placeholder.JPG' className='h-[300px] w-full object-cover rounded-xl'/>
-      <div className=''>
-        <h2 className='font-bold text-2xl'>{trip?.userSelection?.location}</h2>
-      </div>
-      <div>
-       <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-sm'>{trip.userSelection?.noOfDays}Days</h2>
-      </div>
-    </div>
-  )
+function InfoSection({ trip }) {
+    return (
+        <div>
+            <img src='/placeholder.JPG' className='h-[300px] w-full object-cover rounded-xl' />
+            <div className=''>
+                <h2 className='font-bold text-2xl'>{trip?.userSelection?.location}</h2>
+            </div>
+            <div>
+                <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>{trip.userSelection?.noOfDays}Days</h2>
+                <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>{trip.userSelection?.noOfDays}Days</h2>
+                <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500'>{trip.userSelection?.noOfDays}Days</h2>
+            </div>
+        </div>
+    )
 }
 
 export default InfoSection
