d7d255a3edd9a62028e621b9098dc9239566380d
martinondiwa <surekondiwa@gmail.com>
2024-09-20 11:30:57 +0300

    user

--- TripGwiji/src/View-trip/components/infoSection.jsx
+++ TripGwiji/src/View-trip/components/infoSection.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-function InfoSection(trip) {
-  return (
-    <div>
-      <img src='/placeholder.JPG' className='h-[300px] w-full object-cover rounded-xl'/>
-      <div>
-        <h2></h2>
-      </div>
-    </div>
-  )
+function InfoSection({trip}) {
+    return (
+        <div>
+           <>
+           <img src='/placeholder.JPG' className='h-[300px] w-full object-cover rounded-xl' />
+           <div>
+               <h2>{trip?.userSelection?.location?.label}</h2>
+           </div>
+           </>
+        </div>
+    )
 }
 
 export default InfoSection
