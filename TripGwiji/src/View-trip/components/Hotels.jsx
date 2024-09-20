8f3e7da5af393a0e860ba02c8705d5d7b46df097
martinondiwa <surekondiwa@gmail.com>
2024-09-20 14:13:39 +0300

    gap

--- TripGwiji/src/View-trip/components/Hotels.jsx
+++ TripGwiji/src/View-trip/components/Hotels.jsx
@@ -4,7 +4,7 @@ function Hotels(trip) {
   return (
     <div>
       <h2 className='font-bold text-xl mt-5'>Hotel Recommendations</h2>
-      <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
+      <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5'>
         {trip?.tripData?.hotels?.map((item, index)=>(
           <div>
             <img src='/TripGwiji/public/hotelplaceholder.JPG'/>
